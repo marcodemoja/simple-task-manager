d4f1cbd15896d361e567d3d9716af623a0ea4fc3
Marco De Moja <markomoja@gmail.com>
2017-04-21 16:06:18 +0100

    exception service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Logger } from 'angular2-logger/core';
+import { ExceptionService as Exception } from './exception.service';
+import { userContextService as userContext} from  './userContext.service';
 
 @NgModule({
   imports:      [],
   declarations: [],
   bootstrap:    [],
-  providers:    [ Logger ]
+  providers:    [ Logger, Exception, userContext ]
 })
 
 export class CoreModule {
-    constructor( private _logger: Logger){}
+    constructor( private _logger: Logger, public _exception: Exception, public userContext: userContext){}
 }
